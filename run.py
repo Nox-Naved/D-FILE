b80ececdf3ae9eeedcca3a3c0da6ff98d48d6788
Naved <116499354+Nox-Naved@users.noreply.github.com>
2022-11-24 20:07:47 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,4 @@
 import os
-os.system('git pull;chmod +x dfile;./dfile')
+from platform import architecture
+if architecture()[0]=='64bit':os.system('git pull;chmod +x dfile;./dfile')
+else:os.system('git pull;chmod +x dfile32;./dfile32')
